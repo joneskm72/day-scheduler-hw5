519331646f32cc5316b62d148ed59fc585c547e4
Kasey Jones <joneskm72@gmail.com>
2020-07-27 23:50:10 -0400

    Updated get local storage

--- develop/script.js
+++ develop/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   // listen for save button clicks
+  var value = null;
+  var time = null;
 
   $(".saveBtn").on("click", function(event) {
     // get nearby values
     event.preventDefault();
-    const value = $(this).siblings(".description").val();
-    const time = $(this).parent().attr("id");
+    value = $(this).siblings(".description").val();
+    time = $(this).parent().attr("id");
 
     console.log("value:", value);
     console.log("time:", time);
 
     // save the value in localStorage as time
-    localStorage.setItem("value", "time");
-
+    localStorage.setItem(time, value);
+    console.log(localStorage.getItem(time));
   });
 
   function hourUpdater() {
@@ -59,12 +61,53 @@ $(document).ready(function() {
  clearInterval(interval); 
   // set up interval to check if current time needs to be updated
   // which means execute hourUpdater function every 15 seconds
+ 
+  if(localStorage.getItem("hour-9") !== null) {
+    $("#hour-9").children("textarea").text(localStorage.getItem("hour-9"));
+  }
 
-  function getData() {
-    $("textarea").text(localStorage.getItem(value));
-  };
+  if(localStorage.getItem("hour-10") !== null) {
+    $("#hour-10").children("textarea").text(localStorage.getItem("hour-10"));
+  }
+
+  if(localStorage.getItem("hour-11") !== null) {
+    $("#hour-11").children("textarea").text(localStorage.getItem("hour-11"));
+  }
+
+  if(localStorage.getItem("hour-12") !== null) {
+    $("#hour-12").children("textarea").text(localStorage.getItem("hour-12"));
+  }
+
+  if(localStorage.getItem("hour-13") !== null) {
+    $("#hour-13").children("textarea").text(localStorage.getItem("hour-13"));
+  }
+
+  if(localStorage.getItem("hour-14") !== null) {
+    $("#hour-14").children("textarea").text(localStorage.getItem("hour-14"));
+  }
+
+  if(localStorage.getItem("hour-15") !== null) {
+    $("#hour-15").children("textarea").text(localStorage.getItem("hour-15"));
+  }
+
+  if(localStorage.getItem("hour-16") !== null) {
+    $("#hour-16").children("textarea").text(localStorage.getItem("hour-16"));
+  }
+
+  if(localStorage.getItem("hour-17") !== null) {
+    $("#hour-17").children("textarea").text(localStorage.getItem("hour-17"));
+  }
+
+  // $(".time-block").each(function() {
+  //   var tempTime = localStorage.getItem($(this).attr("id"));
+  //   if(localStorage.getItem(tempTime) !== null) {
+  //     $("#" + tempTime).children("textarea").text(localStorage.getItem(tempTime));
+  //     console.log(tempTime);
+  //     console.log(localStorage.getItem(tempTime));
+  //   }
+  // });
   // load any saved data from localStorage
-  
+
 
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
